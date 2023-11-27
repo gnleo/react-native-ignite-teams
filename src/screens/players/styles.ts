6e9f630b04990e750a3e325b6178a92c3e7a2cd4
Leonardo Nunes Goncalves <jamill_gnleo@yahoo.com>
2023-11-27 11:38:46 -0300

    feat: página de jogadores

---
+++ src/screens/players/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components/native";
+import { useTheme } from "styled-components/native";
+ 
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${() => useTheme().COLORS.GRAY_600};
+
+  padding: 24px;
+`
+
+export const Form = styled.View`
+  width: 100%;
+  background-color: ${() => useTheme().COLORS.GRAY_700};
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 6px;
+`
+
+export const HeaderList = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+`
+export const NumbersOfPlayers = styled.Text`
+  color: ${() => useTheme().COLORS.GRAY_200};
+  font-family: ${() => useTheme().FONT_FAMILY.BOLD};
+  font-size: ${() => useTheme().FONT_SIZE.MD}px;
+`
